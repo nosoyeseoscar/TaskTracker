92f20e9d06db5c7c62e70627ee8b909404576b93
Oscar Fuentes <ofuentese@gmail.com>
2024-10-24 13:44:49 -0700

    Fixing Eslint warnings

--- src/components/EnterIcon.jsx
+++ src/components/EnterIcon.jsx
@@ -1,20 +1,20 @@
-export default function EnterIcon({ size, onClick }) {
+export default function EnterIcon ({ size, onClick }) {
   return (
     <svg
       width={`${size}px`}
       height={`${size}px`}
       onClick={onClick}
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
     >
       <path
-        d="M20 7V8.2C20 9.88016 20 10.7202 19.673 11.362C19.3854 11.9265 18.9265 12.3854 18.362 12.673C17.7202 13 16.8802 13 15.2 13H4M4 13L8 9M4 13L8 17"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        d='M20 7V8.2C20 9.88016 20 10.7202 19.673 11.362C19.3854 11.9265 18.9265 12.3854 18.362 12.673C17.7202 13 16.8802 13 15.2 13H4M4 13L8 9M4 13L8 17'
+        stroke='currentColor'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </svg>
-  );
+  )
 }
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
-import { Task } from "./Task";
-
-export default function TaskList({ children }) {
-  //const []
+export default function TaskList ({ children }) {
   return (
     <div>
-      <h2 className="text-slate-200 text-xl pb-2">Task List</h2>
-      <div className="border border-slate-300 rounded-xl p-4">{children}</div>
+      <h2 className='text-slate-200 text-xl pb-2'>Task List</h2>
+      <div className='border border-slate-300 rounded-xl p-4'>{children}</div>
     </div>
-  );
+  )
 }
